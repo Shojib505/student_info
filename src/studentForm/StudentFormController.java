e7675ceb7c3a19f52e43f2ba124365beb43c277e
alish <alishojib12@gmail.com>
2022-04-01 01:43:32 +0600

    student model and from screen running

---
+++ src/studentForm/StudentFormController.java
@@ -0,0 +1,28 @@
+package studentForm;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentFormController implements Initializable {
+
+    public Button updateBtn;
+    public Text textViewAdd;
+    public Text textViewUpdate;
+    public Label lbId;
+    public TextField getName;
+    public TextField getAge;
+    public TextField getAddress;
+    public TextField getEmail;
+    public TextField getProfession;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
